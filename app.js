e96fb8a3adf528922ab9695b4e0c88e4b459cd06
Anantika Agarwal <anantikaag06@gmail.com>
2025-06-28 21:26:03 +0530

    Fix .gitignore and ensure node_modules is excluded

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).
+  mongoose.connect(process.env.MONGODB_URI).
+  then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 const noteSchema = new mongoose.Schema({ text: String });
